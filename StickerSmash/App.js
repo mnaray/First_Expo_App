f34735134a2cbf8108ece7481abd902792414960
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-11-06 10:47:00 +0100

    create basic hello world app

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!!!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name here:"
+        value={name}
+        onChangeText={(val) => setName(val)}
+      />
+      <Button title="Submit" onPress={() => alert(`Hello ${name}`)} />
     </View>
   );
 }
@@ -13,8 +22,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 20,
   },
 });
